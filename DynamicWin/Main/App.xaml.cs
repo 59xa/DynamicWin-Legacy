50e40756d596234dbbebbc90d9e63fac74b9ed32
59xa <59xa@proton.me>
2025-12-22 22:24:27 -0600

    Update version

--- DynamicWin/Main/App.xaml.cs
+++ DynamicWin/Main/App.xaml.cs
@@ -16,7 +16,7 @@ namespace DynamicWin
         public static MMDevice defaultDevice;
         public static MMDevice defaultMicrophone;
 
-        public static string Version => "v1.4.1b";
+        public static string Version => "v1.4.1b1";
         public static string ReleaseStream => "release";
 
         [STAThread]
