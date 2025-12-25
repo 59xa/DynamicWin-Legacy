f97a84dc51dd0cc000947803368cd82b9904c7e9
59xa <59xa@proton.me>
2025-12-25 17:26:59 -0600

    Ensure some overlay menus use the new OpenOverlayMenu logic

--- DynamicWin/Main/RendererMain.cs
+++ DynamicWin/Main/RendererMain.cs
@@ -183,7 +183,7 @@ namespace DynamicWin.Main
             {
                 if (MenuManager.Instance.ActiveMenu is HomeMenu)
                 {
-                    MenuManager.OpenOverlayMenu(new VolumeAdjustMenu(), 100f);
+                    MenuManager.OpenOverlayMenu(new VolumeAdjustMenu(), 2.75f, Res.HomeMenu);
                 }
                 else if (VolumeAdjustMenu.timerUntilClose != null)
                 {
@@ -222,7 +222,7 @@ namespace DynamicWin.Main
                 initialScreenBrightness = BrightnessAdjustMenu.GetBrightness();
                 if (MenuManager.Instance.ActiveMenu is HomeMenu)
                 {
-                    MenuManager.OpenOverlayMenu(new BrightnessAdjustMenu(), 100f);
+                    MenuManager.OpenOverlayMenu(new BrightnessAdjustMenu(), 2.75f, Res.HomeMenu);
                 }
                 else if (BrightnessAdjustMenu.timerUntilClose != null)
                 {
