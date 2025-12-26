b80b825bdb4bcfb1dbec882b95daba101db2c5ca
59xa <59xa@proton.me>
2025-12-26 03:25:24 -0600

    Optimise media fetching logic

--- DynamicWin/Utils/MediaController.cs
+++ DynamicWin/Utils/MediaController.cs
@@ -8,6 +8,7 @@ using System.Windows.Media.Imaging;
 using Windows.Media.Playback;
 using WindowsMediaController;
 using static WindowsMediaController.MediaManager;
+using System.Threading;
 
 namespace DynamicWin.Utils
 {
@@ -18,7 +19,7 @@ namespace DynamicWin.Utils
     *   Author:                 Florian Butz
     *   GitHub:                 https://github.com/FlorianButz
     *   Implementation Date:    3 August 2024
-    *   Last Modified:          3 August 2024 12:46 CET (UTC+1)
+    *   Last Modified:          3 August 2024
     */
 
     public class MediaController
@@ -51,50 +52,117 @@ namespace DynamicWin.Utils
     *    - Allows the fetching of currently playing media, returning its artist name, media title, and its corresponding image.
     *    - Handles metadata mapping through Media class which returns the three mentioned information.
     *    
-    *   Author:                 Megan Park
+    *   Author:                 59xa
     *   GitHub:                 https://github.com/59xa
     *   Implementation Date:    19 May 2025
-    *   Last Modified:          19 May 2025 16:12 KST (UTC+9)
+    *   Last Modified:          26 December 2025
     */
 
-    // TO-DO: Not adding comments for now, as this implementation will be required for v1.4.0b
     public class MediaInfo
     {
         private static MediaInfo? _i;
         private static MediaManager _m;
+        private static bool _started = false;
+        private static readonly SemaphoreSlim _fetchLock = new SemaphoreSlim(1, 1);
+        private static DateTime _lastFetch = DateTime.MinValue;
+        private static readonly TimeSpan _cacheDuration = TimeSpan.FromSeconds(1); // Cache for 1s to reduce work
+
         public static MediaInfo Instance => _i ??= new MediaInfo();
 
         public static Media? Current { get; private set; }
 
         public static async Task<Media?> FetchCurrentMediaAsync()
         {
-            _m = new MediaManager();
-            await _m.StartAsync();
+            // Return cached result if recent
+            if (Current != null && (DateTime.UtcNow - _lastFetch) < _cacheDuration)
+                return Current;
 
-            var _s = _m.GetFocusedSession();
-            if (_s == null) return null;
+            // Ensure manager exists and is started only once
+            if (_m == null)
+                _m = new MediaManager();
 
-            var _p = _s.ControlSession?.TryGetMediaPropertiesAsync()?.GetAwaiter().GetResult();
-            if (_p == null) return null;
-
-            BitmapImage? _i = null;
-            if (_p.Thumbnail != null)
+            if (!_started)
             {
-                using var stream = await _p.Thumbnail.OpenReadAsync();
-                _i = new BitmapImage();
-                _i.BeginInit();
-                _i.StreamSource = stream.AsStreamForRead();
-                _i.CacheOption = BitmapCacheOption.OnLoad;
-                _i.EndInit();
+                try
+                {
+                    await _m.StartAsync();
+                    _started = true;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("MediaManager failed to start: " + ex.Message);
+                    // On failure, avoid retrying too aggressively
+                    _lastFetch = DateTime.UtcNow;
+                    Current = null;
+                    return null;
+                }
             }
 
-            var result = new Media { Title = _p.Title, Artist = _p.Artist, Thumbnail = _i };
+            await _fetchLock.WaitAsync();
+            try
+            {
+                // Re-check cache after acquiring lock
+                if (Current != null && (DateTime.UtcNow - _lastFetch) < _cacheDuration)
+                    return Current;
 
-            Current = result;
+                var _s = _m.GetFocusedSession();
+                if (_s == null)
+                {
+                    Current = null;
+                    _lastFetch = DateTime.UtcNow;
+                    return null;
+                }
 
-            Debug.WriteLine("TITLE: {0}, ARTIST: {1}, IMAGE: {2}", _p.Title, _p.Artist, _p.Thumbnail);
+                // Await media properties instead of blocking
+                var control = _s.ControlSession;
+                if (control == null)
+                {
+                    Current = null;
+                    _lastFetch = DateTime.UtcNow;
+                    return null;
+                }
 
-            return result;
+                var _p = await control.TryGetMediaPropertiesAsync();
+                if (_p == null)
+                {
+                    Current = null;
+                    _lastFetch = DateTime.UtcNow;
+                    return null;
+                }
+
+                BitmapImage? _i = null;
+                if (_p.Thumbnail != null)
+                {
+                    try
+                    {
+                        using var stream = await _p.Thumbnail.OpenReadAsync();
+                        _i = new BitmapImage();
+                        _i.BeginInit();
+                        _i.StreamSource = stream.AsStreamForRead();
+                        _i.CacheOption = BitmapCacheOption.OnLoad;
+                        _i.EndInit();
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("Failed to load thumbnail: " + ex.Message);
+                        _i = null;
+                    }
+                }
+
+                var result = new Media { Title = _p.Title, Artist = _p.Artist, Thumbnail = _i };
+
+                Current = result;
+                _lastFetch = DateTime.UtcNow;
+
+#if DEBUG
+                Debug.WriteLine("[MEDIA CONTROLLER] TITLE: {0}, ARTIST: {1}, IMAGE: {2}", _p.Title, _p.Artist, _p.Thumbnail);
+#endif
+                return result;
+            }
+            finally
+            {
+                _fetchLock.Release();
+            }
         }
     }
 
