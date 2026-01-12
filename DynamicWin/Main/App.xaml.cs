dba8abd9f72bee9f09ea55374a5b371b9fc841a4
59xa <59xa@proton.me>
2026-01-12 13:23:15 -0600

    Bump version

--- DynamicWin/Main/App.xaml.cs
+++ DynamicWin/Main/App.xaml.cs
@@ -19,7 +19,7 @@ namespace DynamicWin
         public static MMDevice defaultDevice;
         public static MMDevice defaultMicrophone;
 
-        public static string Version => "v1.5.0rc4";
+        public static string Version => "v1.5.0rc5";
         public static Channel ReleaseStream => Channel.Canary;
 
         [STAThread]
