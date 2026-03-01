29a9eefefc84fed1947ca9db3a1f115227d8ffb9
59xa <59xa@proton.me>
2026-03-01 01:05:00 -0600

    Mark canary build

--- DynamicWin/Main/App.xaml.cs
+++ DynamicWin/Main/App.xaml.cs
@@ -20,8 +20,8 @@ namespace DynamicWin
         public static MMDevice? defaultDevice;
         public static MMDevice? defaultMicrophone;
 
-        public static string Version => "v1.6.1r";
-        public static Channel ReleaseStream => Channel.Release;
+        public static string Version => "v1.6.2b1";
+        public static Channel ReleaseStream => Channel.Canary;
         public static Architecture ProcessArchitecture => RuntimeInformation.ProcessArchitecture;
 
         [STAThread]
