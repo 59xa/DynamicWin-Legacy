9e542e64cce146d64957d5f7e60a261020ce0597
59xa <59xa@proton.me>
2026-05-31 13:44:11 -0600

    Declare release

--- DynamicWin/Main/App.xaml.cs
+++ DynamicWin/Main/App.xaml.cs
@@ -20,8 +20,8 @@ namespace DynamicWin
         public static MMDevice? defaultDevice;
         public static MMDevice? defaultMicrophone;
 
-        public static string Version => "v1.7.0rc1";
-        public static Channel ReleaseStream => Channel.Canary;
+        public static string Version => "v1.7.0r";
+        public static Channel ReleaseStream => Channel.Release;
         public static Architecture ProcessArchitecture => RuntimeInformation.ProcessArchitecture;
 
         [STAThread]
